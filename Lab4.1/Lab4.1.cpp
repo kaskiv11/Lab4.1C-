ba9cab15e7ff0beeb2e66be4308f638862813526
kaskiv <kaskiv11gmail.com>
2022-04-19 13:37:44 +0300

    Add project files.

---
+++ Lab4.1/Lab4.1.cpp
@@ -0,0 +1,34 @@
+﻿/*
+Дано текстовий файл t. Підрахувати, скільки разів в цьому файлі зустрічається кожний символ, що входить в слово “BASIC” (оформити у вигляді функції).
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int ProcessTXT(char* fname) {
+	ifstream fin(fname);
+	string s;
+	const string keyW = "BASIC";
+	int k = 0;
+	while (getline(fin, s))
+	{
+		for (unsigned i = 0; i < s.length(); i++) {
+			if (s[i] == 'B' || s[i] == 'A' || s[i] == 'S' || s[i] == 'I' || s[i] == 'C') {
+				k++;
+			}
+		}
+	}
+	return k;
+}
+
+int main()
+{
+	char name[6] = "t.txt";
+	cout << "Count of symbols which included in word BASIC: " << ProcessTXT(name) << endl;
+
+	cin.get();
+	return 0;
+}
---
+++ UnitTestLab4.1/UnitTestLab4.1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab4.1/Lab4.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab41
+{
+	TEST_CLASS(UnitTestLab41)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = ProcessTXT("../t.txt");
+			Assert::AreEqual(t, 8);
+		}
+	};
+}
+
+
+
+
